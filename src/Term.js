a3a3d54ef73bcb7d675700d2df5ba282dd0fe3bd
nykki-33 <nykkitou@gmail.com>
2026-04-07 14:18:50 -0400

    Add Terms component for terms and conditions

---
+++ src/Term.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Terms() {
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h2>Terms and Conditions</h2>
+
+      <p>
+        By using Kairos Dance Store, you agree to the following:
+      </p>
+
+      <ul>
+        <li>All products are for personal use only.</li>
+        <li>No refunds after purchase unless item is damaged.</li>
+        <li>Customer information must be accurate.</li>
+        <li>We respect your privacy and do not share your data.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Terms;
